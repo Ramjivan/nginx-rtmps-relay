6bd5240bde1780b424f18a0434bbb4bf7bc000a1
Ramjivan Jangid <ramjeewanj@gmail.com>
2022-03-12 18:10:45 +0000

    fixed typo in v3

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 docker rm -f test
-docker build -t swtest .
-docker run -dti -p 1935:1935 --name test -e PORT=1935 -e STREAMS="rtmp://gopal.streamway.in/live/1474" -e NGINX_RTMP_CTL_API_HOST='' -e AUTH_TOKEN="token" -e ON_PUBLISH_AUTH=True -e UID="8pigHPlupmOt24hmcEfdTONqLTq2" -e DENY_PULL=True -e LOCAL_STREAM=CfgmnOqUIYXXgTmF3NOX  swtest 
+docker build -t uk-london-1.ocir.io/lrqqxehccngb/streamway-relay:test .
+docker push uk-london-1.ocir.io/lrqqxehccngb/streamway-relay:test
+#docker run -dti -p 1935:1935 --name test -e PORT=1935 -e STREAMS="rtmp://gopal.streamway.in/live/1474" -e NGINX_RTMP_CTL_API_HOST='' -e AUTH_TOKEN="token" -e ON_PUBLISH_AUTH=True -e UID="8pigHPlupmOt24hmcEfdTONqLTq2" -e DENY_PULL=True -e LOCAL_STREAM=CfgmnOqUIYXXgTmF3NOX  uk-london-1.ocir.io/lrqqxehccngb/streamway-relay:test 
